2ed852b78ce33dfad4b91dcd4fa8ea315f3a98bb
Bartosz Matusiak <240747@edu.p.lodz.pl>
2023-05-15 15:05:09 +0200

    17. dodanie dokumentacji dla klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -11,29 +11,50 @@ public class DefaultCountingOutRhymer {
 
     private int total = IS_EMPTY_INDICATOR;
 
+    /**
+     *
+     * @return index of last element of numbers array
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * @param in number which will be added to the numbers array
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * @return true if numbers array is empty, false if it is not empty
+     */
     public boolean callCheck() {
         return total == IS_EMPTY_INDICATOR;
     }
 
+    /**
+     * @return true if numbers array is full, false if it is not full
+     */
     public boolean isFull() {
         return total == NUMBERS_MAX_SIZE - 1;
     }
 
+    /**
+     * @return DEFAULT_RETURN_VALUE if numbers array is empty, last number in numbers array if it is not empty
+     */
     protected int peekaboo() {
         if (callCheck())
             return DEFAULT_RETURN_VALUE;
         return numbers[total];
     }
 
+
+    /**
+     * @return DEFAULT_RETURN_VALUE if numbers array is empty, last number in numbers array if it is not empty
+     * it dectrements total
+     */
     public int countOut() {
         if (callCheck())
             return DEFAULT_RETURN_VALUE;
